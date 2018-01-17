1cc13c0eb02bb6f616d1189e0a72192b6454fbe5
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-01-17 20:17:45 +0300

    Add files via upload

---
+++ pyTelegramBotAPI-2.0.5/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup
+from io import open
+
+def readme():
+    with open('README.rst', encoding='utf-8') as f:
+        return f.read()
+
+setup(name='pyTelegramBotAPI',
+      version='2.0.5',
+      description='Python Telegram bot api. ',
+      long_description=readme(),
+      author='eternnoir',
+      author_email='eternnoir@gmail.com',
+      url='https://github.com/eternnoir/pyTelegramBotAPI',
+      packages=['telebot'],
+      license='GPL2',
+      keywords='telegram bot api tools',
+      install_requires=['requests', 'six'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      ]
+      )
