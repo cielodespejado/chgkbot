75339027ad2feea616a61f93aa30fd0fde6551ea
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-01-19 17:33:05 +0300

    Update browse.py

--- browse.py
+++ browse.py
@@ -3,17 +3,33 @@ import urllib
 import requests
 import sys
 
-def get():
-    with urllib.request.urlopen('http://db.chgk.info/random/from_2006-01-01/types1/') as fp:
+def get(year):
+    url = 'http://db.chgk.info/random/from_'+year+'-01-01/types1/'
+    with urllib.request.urlopen(url) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
-        quest = soup.find_all('div','random_question', limit = 1)
-        f = quest[0].get_text()
-        img = quest[0].find_all('img')
-        if img:
-            img = img[0].get('src')
-        
-    words = f.split()
-    I = len(words)
+        question = soup.find_all('div','random_question', limit = 1)
+        answer = question[0].find('div','collapsible collapsed')
+        razdatka = question[0].find('div','razdatka')
+        q = question[0].get_text()
+        if razdatka:
+            razdatka = question[0].find('div','razdatka').extract()
+            r = razdatka.get_text()
+        img_q = question[0].find_all('img')
+        img_a = answer.find_all('img')
+        if img_q:
+            if len(img_q)==len(img_a):
+                img_q = []
+            elif len(img_q)>len(img_a):
+                img_q[:len(img_q)-len(img_a)]
+        if img_q:
+            img_q = img_q[0].get('src')
+        if img_a:
+            img_a = img_a[0].get('src')
+
+    quest = ['']
+    answ = ['']
+            
+    words = q.split()
 
     a = b = c = d = e = f = 0
     for i,word in enumerate(words):
@@ -30,17 +46,16 @@ def get():
         elif word == '1:':
             a = i
 
-    quest = ['']
-    answ = ['']
-    coords = [b,c,d,e,f,I]
+    coords = [b,c,d,e,f,len(words)]
     coords = [i for i in coords if i!=0]
+
+    if razdatka:
+        quest.append(r)
+        quest.append('\n')
 
     for i in range(a+1,b-1):
         quest.append(words[i])
     quest.append('\n')
-
-#    if img:
-#            quest.append(img)
 
     for i in range(0,len(coords)-1):
         for j in range(coords[i],coords[i+1]):
@@ -53,6 +68,4 @@ def get():
     quest = (' '.join(quest))
     answ = (' '.join(answ))
 
-#   print (quest, '\n', answ)
-    return (quest, answ, img)
-
+    return (quest, answ, img_q, img_a)
