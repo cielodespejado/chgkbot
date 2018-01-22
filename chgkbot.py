3a4c3eb74e2a71e19133e2c31535087108138d7f
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-01-22 23:52:44 +0300

    Update chgkbot.py

--- chgkbot.py
+++ chgkbot.py
@@ -186,14 +186,15 @@ def callback_inline(call):
             year2[cid]=call.data
             sent = bot.edit_message_text(chat_id=cid, message_id=yid[cid], text='Интервал сохранён')
           else:
-          keyboard = types.InlineKeyboardMarkup()
-          callback_button = types.InlineKeyboardButton(text='1991-2000', callback_data='int1')
-          callback_button1 = types.InlineKeyboardButton(text='2001-2010', callback_data='int2')
-          callback_button2 = types.InlineKeyboardButton(text='2011-н.в.', callback_data='int3')
-          keyboard.add(callback_button, callback_button1, callback_button2)
-          sent = bot.edit_message_text(chat_id=cid, message_id=yid[cid], text='Конец интервала должен быть больше начала', reply_markup=keyboard)
-          yid[cid] = sent.message_id
-          interval = 'finish'
+            keyboard = types.InlineKeyboardMarkup()
+            callback_button = types.InlineKeyboardButton(text='1991-2000', callback_data='int1')
+            callback_button1 = types.InlineKeyboardButton(text='2001-2010', callback_data='int2')
+            callback_button2 = types.InlineKeyboardButton(text='2011-н.в.', callback_data='int3')
+            keyboard.add(callback_button, callback_button1, callback_button2)
+            sent = bot.edit_message_text(chat_id=cid, message_id=yid[cid], text='Конец интервала должен быть больше начала', reply_markup=keyboard)
+            yid[cid] = sent.message_id
+            interval = 'finish'
+          
                
 
 @bot.message_handler(commands=['timer'])    
