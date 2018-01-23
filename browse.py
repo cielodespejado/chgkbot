0fc3d635ee2f9cf6084a5bb5e6238ab9a9462714
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-01-23 13:58:38 +0300

    Update browse.py

--- browse.py
+++ browse.py
@@ -66,8 +66,8 @@ def get(year1, year2):
         answ.append(words[i])
     answ.append('\n Выбранный диапазон лет: '+year1+'...'+year2+'\n')
     
-    quest = ' '+(' '.join(quest))
-    answ = ' '+(' '.join(answ))
+    quest = (' '.join(quest))
+    answ = (' '.join(answ))
 
     return (quest, answ, img_q, img_a)
 #print (quest,'\n',answ,'\n',img_q,'\n',img_a)
