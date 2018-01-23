84f75eefd5288e90e4db9c8a22d68a9e3cf3be93
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-01-23 11:21:13 +0300

    Update chgkbot.py

--- chgkbot.py
+++ chgkbot.py
@@ -156,7 +156,7 @@ def callback_inline(call):
         for text in range(1991,2001):
           p = types.InlineKeyboardButton(text=str(text), callback_data=str(text))
           button.append(p)
-        keyboard.add(', '.join(x for x in button))
+        keyboard.add(x for x in button))
         sent = bot.edit_message_text(chat_id=cid, message_id=yid[cid], text='Выберите начало интервала', reply_markup=keyboard)
         yid[cid] = sent.message_id
       elif call.data == "int2":
