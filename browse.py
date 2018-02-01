2d77405336081aa966ab3301d87da40215b71fdf
cielodespejado <35187350+cielodespejado@users.noreply.github.com>
2018-02-01 14:11:06 +0300

    Update browse.py

--- browse.py
+++ browse.py
@@ -80,14 +80,12 @@ def get_author(author, year1, year2):
     answ = ['']
     img_q = []
     img_a = []
-    url = 'https://db.chgk.info/search/questions/author_ivolobuev/from_2012-03-25/to_2012-03-25'
-    #url = 'https://db.chgk.info/search/questions/author_'+author+'/types1/sort_date/from_'+year1+'-01-01/to_'+year2+'-12-31/limit10000'
+    url = 'https://db.chgk.info/search/questions/author_'+author+'/types1/sort_date/from_'+year1+'-01-01/to_'+year2+'-12-31/limit10000'
     with urllib.request.urlopen(url) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
         allquests = soup.find_all('div','question')
         tournaments = soup.find_all('dt','title')
-        #N = random.randint(0,len(allquests)-1)
-        N = 6
+        N = random.randint(0,len(allquests)-1)
         tournament = tournaments[N].get_text()
         razdatka = allquests[N].find('div','razdatka')
         if razdatka:
